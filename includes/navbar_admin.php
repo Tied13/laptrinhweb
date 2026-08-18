d39728726dfad3426f43c66b04ebb287a6273c5c
phnng10 <nguyenphn1441@ut.edu.vn>
2026-08-18 22:52:54 +0700

    Update navbar_admin.php

---
+++ includes/navbar_admin.php
@@ -0,0 +1,12 @@
+<!-- admin_nav.php -->
+<div class="navbar">
+  <div class="logo">Cửa hàng bán gấu bông</div>
+  <div class="nav-center">
+    <a href="admin_inventory.php">Quản lý kho</a>
+    <a href="admin_import.php">Nhập hàng</a>
+    <a href="admin_stock_report.php">Báo cáo tồn kho</a>
+  </div>
+  <div class="icons">
+    <a class="logout-link" href="../logout.php">Đăng xuất</a>
+  </div>
+</div>
