0d15d0706d3bca02d9dac72b041a6899e04a7061
phnng10 <nguyenphn1441@ut.edu.vn>
2026-08-18 22:51:49 +0700

    Update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="admin-footer">
+    <p>
+        TeddyLand – Quản lý cửa hàng gấu bông
+    </p>
+    <div class="admin-footer-links">
+        <a href="#">Trợ giúp</a>
+        <a href="#">Liên hệ</a>
+    </div>
+</footer>
