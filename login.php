83eb1cc2f53e13dc141281e108fdef411f06c9f5
phnng10 <nguyenphn1441@ut.edu.vn>
2026-08-18 22:53:40 +0700

    Update login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập - Cửa hàng bán Gấu bông</title>
+
+    <link rel="stylesheet" href="assets/css/admin.css">
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+    <main class="auth-container">
+        <section class="login">
+            <h2>Đăng nhập</h2>
+            <form action="#" method="post">
+                <input type="text" name="username" placeholder="Tên đăng nhập" required />
+                <input type="password" name="password" placeholder="Mật khẩu" required />
+                <button type="submit">Đăng nhập</button>
+            </form>
+            <p>Bạn chưa có tài khoản?
+                <a href="register.php">Đăng ký ngay</a>
+            </p>
+        </section>
+    </main>
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
