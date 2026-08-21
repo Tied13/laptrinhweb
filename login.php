25dc0c5fc82ace6f7739ed9d3ba15f6cfce99850
phnng10 <nguyenphn1441@ut.edu.vn>
2026-08-21 22:28:25 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Đăng ký - Cửa hàng bán gấu bông</title>
-  <link rel="stylesheet" href="assets/css/admin.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập - Cửa hàng bán Gấu bông</title>
+    <link rel="stylesheet" href="assets/css/admin.css">
 </head>
 <body>
     <?php include("includes/header.php"); ?>
-    <section class="register">
-        <h2>Đăng ký tài khoản</h2>
-        <form action="controllers/AuthController.php" method="post">
-        <input type="text" name="fullname" placeholder="Họ và tên" required />
-        <input type="text" name="username" placeholder="Tên đăng nhập" required />
-        <input type="email" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Mật khẩu" required />
-        <input type="password" name="confirm" placeholder="Nhập lại mật khẩu" required />
-        <button type="submit">Đăng ký</button>
-    </form>
-    <p>Bạn đã có tài khoản? 
-        <a href="login.php">Đăng nhập</a>
-    </p>
-    </section>
-<?php include("includes/footer.php"); ?>
+    <main class="auth-container">
+        <section class="login">
+            <h2>Đăng nhập</h2>
+            <form action="controllers/AuthController.php" method="post">
+                <input type="text" name="username" placeholder="Tên đăng nhập" required />
+                <input type="password" name="password" placeholder="Mật khẩu" required />
+                <button type="submit">Đăng nhập</button>
+            </form>
+            <p>Bạn chưa có tài khoản?
+                <a href="register.php">Đăng ký ngay</a>
+            </p>
+        </section>
+    </main>
+    <?php include("includes/footer.php"); ?>
 </body>
 </html>
