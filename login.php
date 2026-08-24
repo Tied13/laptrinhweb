d18d51e3dbf03d4e41d788dd6299335f6a566603
phnng10 <nguyenphn1441@ut.edu.vn>
2026-08-24 16:56:28 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng nhập - Cửa hàng bán Gấu bông</title>
-    <link rel="stylesheet" href="assets/css/admin.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <?php include("includes/header.php"); ?>
