2b3f02f139ab75d3470d71013555d89672a1b7bb
minhtam1201 <tamvtm1985@ut.edu.vn>
2026-08-25 22:09:04 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liên hệ - Cửa hàng bán gấu bông</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
 <?php include("includes/header.php"); ?>
