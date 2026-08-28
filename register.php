79308645beab7552cee4f4dbed5e1d257f0c63af
phnng10 <nguyenphn1441@ut.edu.vn>
2026-08-28 11:16:16 +0700

    Update register.php

--- register.php
+++ register.php
@@ -4,23 +4,25 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Đăng ký - Cửa hàng bán gấu bông</title>
-  <link rel="stylesheet" href="assets/css/admin.css"/>
+  <link rel="stylesheet" href="assets/css/style.css"/>
 </head>
 <body>
     <?php include("includes/header.php"); ?>
-    <section class="register">
-        <h2>Đăng ký tài khoản</h2>
-        <form action="controllers/AuthController.php" method="post">
-        <input type="text" name="fullname" placeholder="Họ và tên" required />
-        <input type="text" name="username" placeholder="Tên đăng nhập" required />
-        <input type="email" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Mật khẩu" required />
-        <input type="password" name="confirm" placeholder="Nhập lại mật khẩu" required />
-        <button type="submit">Đăng ký</button>
-    </form>
-    <p>Bạn đã có tài khoản? 
-        <a href="login.php">Đăng nhập</a>
-    </p>
+    <section class="auth-container">
+        <div class="login">
+            <h2>Đăng ký tài khoản</h2>
+            <form action="controllers/AuthController.php" method="post">
+                <input type="text" name="fullname" placeholder="Họ và tên" required />
+                <input type="text" name="username" placeholder="Tên đăng nhập" required />
+                <input type="email" name="email" placeholder="Email" required />
+                <input type="password" name="password" placeholder="Mật khẩu" required />
+                <input type="password" name="confirm" placeholder="Nhập lại mật khẩu" required />
+                <button type="submit">Đăng ký</button>
+        </form>
+        <p>Bạn đã có tài khoản?
+            <a href="login.php">Đăng nhập</a>
+        </p>
+    </div>
     </section>
 <?php include("includes/footer.php"); ?>
 </body>
